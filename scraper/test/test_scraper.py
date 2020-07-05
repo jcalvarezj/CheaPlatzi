b9f14720d674bbdc82dd22921c43a698be1f62f5
J. Alvarez <jcalvarezj@gmail.com>
2020-07-05 18:37:30 -0500

    Implemented base of ColombiaGamer scraper

--- scraper/test/test_scraper.py
+++ scraper/test/test_scraper.py
@@ -4,17 +4,19 @@ This module performs unit tests on the scraper module
 import os
 import json
 import pytest
-from ..utils.spiders import OLXSpider
-from ..utils.constants import OLXConfig as OLX
+from ..utils.spiders import OLXSpider, CGamerSpider
+from ..utils.constants import OLXConfig as OLX, ColombiaGamerConfig as CGamer
 from scrapy.crawler import CrawlerProcess
 
 
-def cleanup(created_file_path = None):
+def cleanup(created_file_path):
     """
     Deletes a file the test process created
     """
-    if created_file_path:
+    try:
         os.remove(created_file_path)
+    except OSError:
+        pass
 
 
 def olx_setup():
@@ -28,20 +30,52 @@ def olx_setup():
     process.start()
 
 
-def test_olx_scrapper_happy_path_json_data_exported():
+def cgamer_setup():
+    """
+    Initializes the required conditions for testing on ColombiaGamer's site
+    """
+    fileURIs = [f'file:{CGamer.TEST_PATH.value}/{file_name}' for file_name 
+                in CGamer.TEST_FILES.value]
+    print(f'SCRAPEANDO A : {fileURIs}')
+    process = CrawlerProcess()
+    process.crawl(CGamerSpider, start_urls = fileURIs)
+    process.start()
+
+
+# def test_olx_scrapper_happy_path_json_data_exported():
+#     """
+#     This test case checks if the scraper generates the right json file after
+#     scraping a mock with OLX's site structure
+#     """
+#     file_path = f'{OLX.EXPORT_FILE_PATH.value}'
+#     cleanup(file_path)
+#     olx_setup()
+
+#     assert os.path.exists(file_path), f'expected the file {file_path} to exist'
+
+#     with open(file_path) as json_file:
+#         data = json.load(json_file)
+        
+#         assert data == OLX.TEST_PRODUCTS.value, \
+#                 'the exported json file does not match the expected result'
+
+#     cleanup(file_path)
+
+
+def test_cgamer_scrapper_happy_path_json_data_exported():
     """
     This test case checks if the scraper generates the right json file after
-    scraping a mock with OLX structure
+    scraping a mock with CGamer's site structure
     """
-    olx_setup()
-
-    file_path = f'{OLX.EXPORT_FILE_PATH.value}'
+    file_path = f'{CGamer.EXPORT_FILE_PATH.value}'
+    cleanup(file_path)
+    cgamer_setup()
 
     assert os.path.exists(file_path), f'expected the file {file_path} to exist'
 
     with open(file_path) as json_file:
         data = json.load(json_file)
-        assert data == OLX.TEST_PRODUCTS.value, \
+        assert data == CGamer.TEST_PRODUCTS.value, \
                 'the exported json file does not match the expected result'
 
     cleanup(file_path)
--- scraper/utils/constants.py
+++ scraper/utils/constants.py
@@ -6,31 +6,45 @@ import urllib.parse
 from enum import Enum
 
 
-def _get_test_products(path):
+def _get_test_products(path, html_desc = False):
     """
     Creates a list of test products
     """
+    EXPECTED_DESCS = [
+        'Ultima consola de Nintendo. Con controles extra.',
+        'Completamente nuevo. Color blanco. 33 juegos.',
+        'Consola de Microsoft con 7 juegos.'
+    ]
     return [
         {
             'name': 'Nintendo Switch',
-            'description': 'Ultima consola de Nintendo. Con controles extra.',
+            'description': EXPECTED_DESCS[0] if not html_desc
+                           else f'<p>{EXPECTED_DESCS[0]}</p>',
             'price': '$ 1.000.000',
-            'image': urllib.parse.quote(f'file:///{path}/switch.jpg', safe = '/:'),
-            'url': urllib.parse.quote(f'file:///{path}/switch_mock.html', safe = '/:')
+            'image': urllib.parse.quote(f'file:///{path}/switch.jpg',
+                                        safe = '/:'),
+            'url': urllib.parse.quote(f'file:///{path}/switch_mock.html',
+                                      safe = '/:')
         },
         {
             'name': 'PlayStation 4',
-            'description': 'Completamente nuevo. Color blanco. 33 juegos.',
+            'description': EXPECTED_DESCS[1] if not html_desc
+                           else f'<p>{EXPECTED_DESCS[1]}</p>',
             'price': '$ 1.350.000',
-            'image': urllib.parse.quote(f'file:///{path}/play.jpg', safe = '/:'),
-            'url': urllib.parse.quote(f'file:///{path}/playstation_mock.html', safe = '/:')
+            'image': urllib.parse.quote(f'file:///{path}/play.jpg',
+                                        safe = '/:'),
+            'url': urllib.parse.quote(f'file:///{path}/playstation_mock.html',
+                                      safe = '/:')
         },
         {
             'name': 'Xbox One S',
-            'description': 'Consola de Microsoft con 7 juegos.',
+            'description': EXPECTED_DESCS[2] if not html_desc
+                           else f'<p>{EXPECTED_DESCS[2]}</p>',
             'price': '$ 1.550.000',
-            'image': urllib.parse.quote(f'file:///{path}/xbox.jpg', safe = '/:'),
-            'url': urllib.parse.quote(f'file:///{path}/xbox_mock.html', safe = '/:')
+            'image': urllib.parse.quote(f'file:///{path}/xbox.jpg',
+                                        safe = '/:'),
+            'url': urllib.parse.quote(f'file:///{path}/xbox_mock.html',
+                                      safe = '/:')
         }
     ]
 
@@ -58,9 +72,9 @@ class OLXConfig(Enum):
     """
     This enum provides configuration constants for OLX scraping
     """
-    PRODUCTS_URL = 'https://www.olx.com.co/video-juegos-consolas_c1022'    
+    PRODUCTS_URL = 'https://www.olx.com.co/video-juegos-consolas_c1022'
     SPIDER_NAME = 'olxspider'
-    BASE_DOMAIN = 'olx.com.co'
+    ITEM_CLASS = 'itemBox'
     EXPORT_FILE_PATH = 'export/olx_items.json'
     RIGHT_SECT_CLASS = '_2wMiF'
     LEFT_SECT_CLASS = 'CBG3S'
@@ -68,3 +82,25 @@ class OLXConfig(Enum):
     TEST_PATH = f'{os.getcwd()}/scraper/test/olx_mocks'
     TEST_FILE = 'olx_mock.html'
     TEST_PRODUCTS = _get_test_products(TEST_PATH)
+
+
+class ColombiaGamerConfig(Enum):
+    """
+    This enum provides configuration constants for ColombiaGamer scraping
+    """
+    PRODUCT_URLS = [
+        'https://www.colombiagamer.com.co/productos/xbox-one',
+        'https://www.colombiagamer.com.co/productos/playstation-4',
+        'https://www.colombiagamer.com.co/productos/nintendo-switch'
+    ]
+    SPIDER_NAME = 'cgamerspider'
+    EXPORT_FILE_PATH = 'export/cgamer_items.json'
+    ITEM_CLASS = 'product-container'
+    IMG_CLASS = 'main-image'
+    TITLE_CLASS = 'vm-product-title'
+    PRICE_CLASS = 'PricesalesPrice'
+    DESC_CLASS = 'product-description'
+    TEST_PATH = f'{os.getcwd()}/scraper/test/cgamer_mocks'
+    TEST_FILES = ['cgamer_switch_mock.html', 'cgamer_playstation_mock.html',
+                  'cgamer_xbox_mock.html']
+    TEST_PRODUCTS = _get_test_products(TEST_PATH, True)
