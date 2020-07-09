4f124df8f0fb1c37500123148e6e020550e02e44
Jesús Wisord <wisord.autumn@gmail.com>
2020-07-09 12:09:38 -0500

    Fixing style errors in test_scraper

--- scraper/test/test_scraper.py
+++ scraper/test/test_scraper.py
@@ -75,43 +75,43 @@ def sears_setup():
     process.start()
 
 
-# def test_olx_scrapper_happy_path_json_data_exported():
-#     """
-#     This test case checks if the scraper generates the right json file after
-#     scraping a mock with OLX's site structure
-#     """
-#     file_path = f'{OLX.EXPORT_FILE_PATH.value}'
-#     cleanup(file_path)
-#     olx_setup()
+def test_olx_scrapper_happy_path_json_data_exported():
+    """
+    This test case checks if the scraper generates the right json file after
+    scraping a mock with OLX's site structure
+    """
+    file_path = f'{OLX.EXPORT_FILE_PATH.value}'
+    cleanup(file_path)
+    olx_setup()
 
-#     assert os.path.exists(file_path), f'expected the file {file_path} to exist'
+    assert os.path.exists(file_path), f'expected the file {file_path} to exist'
 
-#     with open(file_path) as json_file:
-#         data = json.load(json_file)
+    with open(file_path) as json_file:
+        data = json.load(json_file)
         
-#         assert data == OLX.TEST_PRODUCTS.value, \
-#                 'the exported json file does not match the expected result'
+        assert data == OLX.TEST_PRODUCTS.value, \
+                'the exported json file does not match the expected result'
 
-#     cleanup(file_path)
+    cleanup(file_path)
 
 
-# def test_cgamer_scrapper_happy_path_json_data_exported():
-#     """
-#     This test case checks if the scraper generates the right json file after
-#     scraping a mock with CGamer's site structure
-#     """
-#     file_path = f'{CGamer.EXPORT_FILE_PATH.value}'
-#     cleanup(file_path)
-#     cgamer_setup()
+def test_cgamer_scrapper_happy_path_json_data_exported():
+    """
+    This test case checks if the scraper generates the right json file after
+    scraping a mock with CGamer's site structure
+    """
+    file_path = f'{CGamer.EXPORT_FILE_PATH.value}'
+    cleanup(file_path)
+    cgamer_setup()
 
-#     assert os.path.exists(file_path), f'expected the file {file_path} to exist'
+    assert os.path.exists(file_path), f'expected the file {file_path} to exist'
 
-#     with open(file_path) as json_file:
-#         data = json.load(json_file)
-#         assert data == CGamer.TEST_PRODUCTS.value, \
-#                 'the exported json file does not match the expected result'
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+        assert data == CGamer.TEST_PRODUCTS.value, \
+                'the exported json file does not match the expected result'
 
-#     cleanup(file_path)
+    cleanup(file_path)
 
 def test_gamepl_scrapper_happy_path_json_data_exported():
     """
@@ -129,40 +129,40 @@ def test_gamepl_scrapper_happy_path_json_data_exported():
         assert data == GamePl.TEST_PRODUCTS.value, \
                 'the exported json file does not match the expected result'
 
-    # cleanup(file_path)
+    cleanup(file_path)
 
-# def test_mixup_scrapper_happy_path_json_data_exported():
-#     """
-#     This test case checks if the scraper generates the right json file after
-#     scraping a mock with Mixup's site structure
-#     """
-#     file_path = f'{MUC.EXPORT_FILE_PATH.value}'
-#     cleanup(file_path)
-#     mixup_setup()
+def test_mixup_scrapper_happy_path_json_data_exported():
+    """
+    This test case checks if the scraper generates the right json file after
+    scraping a mock with Mixup's site structure
+    """
+    file_path = f'{MUC.EXPORT_FILE_PATH.value}'
+    cleanup(file_path)
+    mixup_setup()
 
-#     assert os.path.exists(file_path), f'expected the file {file_path} to exist'
+    assert os.path.exists(file_path), f'expected the file {file_path} to exist'
 
-#     with open(file_path) as json_file:
-#         data = json.load(json_file)
-#         assert data == MUC.TEST_PRODUCTS.value, \
-#                 'the exported json file does not match the expected result'
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+        assert data == MUC.TEST_PRODUCTS.value, \
+                'the exported json file does not match the expected result'
 
-#     cleanup(file_path)
+    cleanup(file_path)
 
-# def test_sears_scrapper_happy_path_json_data_exported():
-#     """
-#     This test case checks if the scraper generates the right json file after
-#     scraping a mock with Sears's site structure
-#     """
-#     file_path = f'{SEA.EXPORT_FILE_PATH.value}'
-#     cleanup(file_path)
-#     sears_setup()
+def test_sears_scrapper_happy_path_json_data_exported():
+    """
+    This test case checks if the scraper generates the right json file after
+    scraping a mock with Sears's site structure
+    """
+    file_path = f'{SEA.EXPORT_FILE_PATH.value}'
+    cleanup(file_path)
+    sears_setup()
 
-#     assert os.path.exists(file_path), f'expected the file {file_path} to exist'
+    assert os.path.exists(file_path), f'expected the file {file_path} to exist'
 
-#     with open(file_path) as json_file:
-#         data = json.load(json_file)
-#         assert data == SEA.TEST_PRODUCTS.value, \
-#                 'the exported json file does not match the expected result'
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+        assert data == SEA.TEST_PRODUCTS.value, \
+                'the exported json file does not match the expected result'
 
-#     cleanup(file_path)
+    cleanup(file_path)
